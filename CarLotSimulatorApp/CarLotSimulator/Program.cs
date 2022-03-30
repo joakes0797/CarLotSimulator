102fb178a1a6ce4938301396d897349637303d28
Jill Oakes <jericho_rules@hotmail.com>
2022-03-30 12:11:38 -0400

    Complete Exercise 2 Bonus 1

--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -13,14 +13,42 @@ namespace CarLotSimulator
             //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
             //The methods should take one string parameter: the respective noise property
 
-
-            //Now that the Car class is created we can instanciate 3 new cars
+            //Now that the Car class is created we can instantiate 3 new cars
             //Set the properties for each of the cars
             //Call each of the methods for each car
 
             //*************BONUS*************//
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
+
+            //------------------------------ Dot Notation (instantiating, then assigning values to properties)
+            var jillCar = new Car();
+            jillCar.Year = 2014;
+            jillCar.Make = "Toyota";
+            jillCar.Model = "Camry";
+            jillCar.EngineNoise = "loud on highway, quiet on creep";
+            jillCar.HonkNoise = "honk!";
+            jillCar.IsDriveable = true;
+
+            //------------------------------ Object Initializer Syntax (assigning properties at time of instantiation)
+            var benCar = new Car()
+            {
+                Year = 2018,
+                Make = "Tesla",
+                Model = "Model 3",
+                EngineNoise = "zoom zoom",
+                HonkNoise = "HOOOONNKKKK!",
+                IsDriveable = true
+            };
+
+            //------------------------------- Constructor (in-line instantiating with constructor)
+            var davidCar = new Car(1993, "Chevy", "Cheynne", "lurba lurba lurba", "AIR HORN!", false);
+
+
+            jillCar.MakeEngineNoise(jillCar.EngineNoise);
+            benCar.MakeEngineNoise(benCar.EngineNoise);
+            davidCar.MakeEngineNoise(davidCar.EngineNoise);
+            
 
             //*************BONUS X 2*************//
 
